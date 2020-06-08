5fc5f90767b4c318f6b04c76224cbc868087ea46
Sumanshu Nankana <sumanshunankana@gmail.com>
2020-06-08 14:42:31 +0100

    power of two

--- June_Challenge_2020/8 # Power of Two.py
+++ June_Challenge_2020/8 # Power of Two.py
@@ -56,12 +56,16 @@ print(obj.isPowerOfTwo(n))
 
 # Accepted in Leetcode 
 
+# ====================================================
+
 class Solution(object):
     def isPowerOfTwo(self, n):
         if n > 0 and (n & n-1) == 0:
             return True
         else:
             return False
+
+# ====================================================
 
 
 
