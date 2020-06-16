7a145ed87caa45861c67045fe7ba7cc978778d4b
Sumanshu Nankana <sumanshunankana@gmail.com>
2020-06-16 21:37:58 +0100

    Validate IP address

---
+++ June_Challenge_2020/15 # Search in a Binary Search Tree.py
@@ -0,0 +1,73 @@
+# Given the root node of a binary search tree (BST) and a value. 
+# You need to find the node in the BST that the node's value equals the given value. 
+# Return the subtree rooted with that node. 
+# If such node doesn't exist, you should return NULL.
+
+# For example, 
+
+# Given the tree:
+#         4
+#        / \
+#       2   7
+#      / \
+#     1   3
+
+# And the value to search: 2
+# You should return this subtree:
+
+#       2     
+#      / \   
+#     1   3
+
+# In the example above, if we want to search the value 5, 
+# since there is no node with value 5, we should return NULL.
+
+# Note that an empty tree is represented by NULL, 
+# therefore you would see the expected output (serialized tree format) as [], not null.
+
+# =====================================================================
+# Recursive Approach
+# Accepted in Leetcode
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        if root.val == val:
+            return root
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
+            
+        return None
+    
+# =========================================================================
+# Iterative approach
+# accepted in Leetcode
+
+class Solution(object):
+    def searchBST(self, root, val):
+        current = root
+        while current is not None:
+            if current.val == val:
+                return current
+            elif current.val > val:
+                current = current.left
+            else:
+                current = current.right
+        return None
+
+# =============================================================================
