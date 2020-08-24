22c07b8d41d91b9309b5adca810a26122dc41566
Sumanshu Nankana <sumanshunankana@gmail.com>
2020-08-24 20:28:36 +0100

    Sum of Left Leaves

--- Aug_Challenge_2020/24 # Sum of Left Leaves.py
+++ Aug_Challenge_2020/24 # Sum of Left Leaves.py
@@ -40,3 +40,32 @@ class Solution(object):
         return self.sum
 
 # =============================================================================
+# Another Approach
+# Accepted in Leetcode
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.sum = 0
+        def dfs(root):
+            if root is None: 
+                return 0
+            if root.left:
+                if root.left.left is None and root.left.right is None:
+                    self.sum += root.left.val
+            
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return self.sum
+
+# ==============================================================================
