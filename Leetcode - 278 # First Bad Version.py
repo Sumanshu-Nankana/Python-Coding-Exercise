4841dd0de75d5a4ea69b81ebe26e1ec65a173ff6
Sumanshu Nankana <sumanshunankana@gmail.com>
2023-01-12 23:21:23 +0000

    first bad version

---
+++ Leetcode - 278 # First Bad Version.py
@@ -0,0 +1,20 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersionBruteForce(self, n: int) -> int:
+        for i in range(1, n+1):
+            if isBadVersion(i):
+                return i
+
+    def firstBadVersionOptimalApproach(self, n: int) -> int:
+        left = 1
+        right = n
+
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
