f4bcc3fe2ed20e081e93ccdd42350fac66af3382
Sumanshu Nankana <sumanshunankana@gmail.com>
2023-04-08 15:37:37 +0100

    Happy Number

---
+++ Leetcode - 202 # Happy Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sumDigitsSquare(self, n):
+        digit_sq_sum = 0
+        while n != 0:
+            n, digit = divmod(n, 10)
+            digit_sq_sum = digit_sq_sum + digit ** 2
+
+        return digit_sq_sum
+
+    def isHappy(self, n: int) -> bool:
+        visited = set()
+        while n not in visited:
+            visited.add(n)
+            n = self.sumDigitsSquare(n)
+            if n == 1:
+                return True
+        return False
