17d56803188dbf20e8b580f99f106b714eacaba9
Sumanshu Nankana <sumanshunankana@gmail.com>
2023-06-15 22:59:10 +0100

    Valid Anagram

---
+++ Leetcode - 242 # Valid Anagram.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        s_dict = {}
+        t_dict = {}
+
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+
+        for char in t:
+            t_dict[char] = t_dict.get(char, 0) + 1
+
+        for key, value in s_dict.items():
+            if t_dict.get(key, 0) != value:
+                return False
+
+        return True
+
+
+# using one dictionary
+class Solution:
+    def isAnagram(self, s, t) -> bool:
+
+        if len(s) != len(t): return False
+
+        s_dict = {}
+
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+
+        for char in t:
+            s_dict[char] = s_dict.get(char, 0) - 1
+            if s_dict[char] < 0:
+                return False
+
+        return True
