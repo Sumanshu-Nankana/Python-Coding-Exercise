6401f4c668e49ee00daff5140ea4e6fe60ba081a
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-05-10 14:42:43 +0100

    added solution for ACM_ICPC_Team

---
+++ HackerRank/ACM_ICPC_Team.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'acmTeam' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY topic as parameter.
+#
+
+def acmTeam(topic):
+    max_topics = 0
+    team_count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            topics_known = 0
+            for x, y in zip(topic[i], topic[j]):
+                if x == "1" or y == "1":
+                    topics_known += 1
+
+            if topics_known > max_topics:
+                max_topics = topics_known
+                team_count = 1
+            elif topics_known == max_topics:
+                team_count += 1
+
+    return [max_topics, team_count]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    topic = []
+
+    for _ in range(n):
+        topic_item = input()
+        topic.append(topic_item)
+
+    result = acmTeam(topic)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
