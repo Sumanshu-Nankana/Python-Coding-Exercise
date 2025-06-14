11817a40f58b1932b4bd16dff521ddb43672f0a9
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-06-14 19:11:22 +0100

    added solution for counting sort 1

---
+++ HackerRank/Counting_Sort_1.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+# https://www.hackerrank.com/challenges/countingsort1/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Create a list of 100 zeros
+    count = [0] * 100
+
+    # count the frequency of each number in arr
+    for num in arr:
+        count[num] += 1
+
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
