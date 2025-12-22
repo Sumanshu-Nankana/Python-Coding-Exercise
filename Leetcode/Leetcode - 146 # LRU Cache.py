a6425784f57aaa0c00ffe5f0166d457870955eef
Sumanshu Nankana <sumanshunankana@gmail.com>
2025-12-22 23:44:13 +0530

    added solution with OrderedDict

--- Leetcode/Leetcode - 146 # LRU Cache.py
+++ Leetcode/Leetcode - 146 # LRU Cache.py
@@ -83,6 +83,30 @@ class LRUCacheWithoutDequeSimple:
             self.hash_map[key] = value
 
 
+
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        # Move the accessed key to the end to mark it as most recently used
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            # last=False pops the first (oldest) item
+            self.cache.popitem(last=False)
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
